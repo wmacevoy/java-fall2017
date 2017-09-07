d160c6a52bcdf136eb058059ce4898d42f33e4ab
Warren MacEvoy <wmacevoy@gmail.com>
2017-09-07 10:33:44 -0600

    boilerplate

---
+++ src/java/fall2017/JavaFall2017.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.fall2017;
+
+/**
+ *
+ * @author wmacevoy
+ */
+public class JavaFall2017 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
